3bc7eff63e58a51a45477225cfb3b3420048f8d9
jarpar <partyka.jaroslaw@gmail.com>
2020-03-12 23:12:50 +0100

    added console outprint

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -13,3 +13,9 @@ public class App {
         //System.out.println("Hello World!");
     }
 }
+/*
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -javaagent:/snap/intellij-idea-community/208/lib/idea_rt.jar=37299:/snap/intellij-idea-community/208/bin -Dfile.encoding=UTF-8 -classpath /home/user/IdeaProjects/Fun-with-Maven/target/classes:/home/user/.m2/repository/org/slf4j/slf4j-api/1.7.25/slf4j-api-1.7.25.jar:/home/user/.m2/repository/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar io.github.mat3e.App
+[main] INFO io.github.mat3e.App - Hello World
+
+Process finished with exit code 0
+ */
