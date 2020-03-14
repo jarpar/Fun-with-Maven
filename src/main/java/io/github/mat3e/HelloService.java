32661ded28ff9e7f0236c42e4f33c4c6c6b55602
jarpar <Mamotoja88>
2020-03-14 19:27:13 +0100

    created repository

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -4,10 +4,14 @@ import java.util.Optional;
 
 public class HelloService {
     static final String FALLBACK_NAME = "world";
+    static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
 
     String prepareGreeting(String name) {
+        return prepareGreeting(name, FALLBACK_LANG.getId());
+    }
+
+    String prepareGreeting(String name, Long langId) {
         return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
 
     }
-
 }
