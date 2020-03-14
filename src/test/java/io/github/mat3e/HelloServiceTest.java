98207e2a4083580fdaa06635560586e6abc50956
jarpar <partyka.jaroslaw@gmail.com>
2020-03-14 15:35:27 +0100

    created HelloServiceTest

---
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.mat3e;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloServiceTest {
+    @Test
+    public void testDemo() throws Exception {
+        assertTrue(true);
+    }
+}
