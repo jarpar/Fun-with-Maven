339ab1e7022eb59283ff489c1c8ee41ad0453606
jarpar <partyka.jaroslaw@gmail.com>
2020-03-14 15:41:52 +0100

    completed test HelloServiceTest

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -2,11 +2,18 @@ package io.github.mat3e;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HelloServiceTest {
+    private HelloService SUT = new HelloService();// system under test
+
     @Test
-    public void testDemo() throws Exception {
-        assertTrue(true);
+    public void test_null_prepareGreeting_returnsFallbackValue() {
+        //given + when
+        var result = SUT.prepareGreeting(null);
+
+        //then
+        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
     }
 }
