7ea5a78a45e42457be05b011d42a029a166966f8
jarpar <Mamotoja88>
2020-03-14 11:49:44 +0100

    fixed Embedded Jetty does not find Annotated Servlet

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,7 +1,10 @@
 package io.github.mat3e;
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.*;
 
 /**
  * Hello world!
@@ -11,6 +14,19 @@ public class App {
         var webapp = new WebAppContext();
         webapp.setResourceBase("src/main/webapp");
         webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+
 
 //        webapp.addServlet(HelloServlet.class, "/api/*");
         var server = new Server(8080);
