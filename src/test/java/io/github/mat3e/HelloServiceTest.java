47176e9ed4ebda9535d65482a966e7c1150189be
jarpar <partyka.jaroslaw@gmail.com>
2020-03-14 16:09:10 +0100

    completed test HelloServiceTest

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -9,11 +9,23 @@ public class HelloServiceTest {
     private HelloService SUT = new HelloService();// system under test
 
     @Test
-    public void test_null_prepareGreeting_returnsFallbackValue() {
+    public void test_prepareGreeting_null_returnsGreetingWithFallback() {
         //given + when
         var result = SUT.prepareGreeting(null);
 
         //then
         assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
     }
+
+    @Test
+    public void test_prepareGreeting_name_returnsGreetingWithName() {
+        //given
+        var name = "test";
+        // when
+        var result = SUT.prepareGreeting(name);
+
+        //then
+        assertEquals("Hello " + name + "!", result);
+    }
+
 }
